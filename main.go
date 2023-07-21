d02ac898736df56536b3d6e4bd3eddcf7cb62c61
Dai Yeqi <1146156+daiyeqi@users.noreply.github.com>
2023-07-21 20:17:23 +0800

    Add amazon primevideo...

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ var (
 	dataPath     = flag.String("datapath", filepath.Join("./", "data"), "Path to your custom 'data' directory")
 	datName      = flag.String("datname", "geosite.dat", "Name of the generated dat file")
 	outputPath   = flag.String("outputpath", "./publish", "Output path to the generated files")
-	exportLists  = flag.String("exportlists", "netflix,youtube,hbo,disney,roku,spotify,twitch,xvideos,pornhub,dmm,dlsite,telegram,speedtest,openai,playstation,nintendo,xbox", "Lists to be exported in plaintext format, separated by ',' comma")
+	exportLists  = flag.String("exportlists", "netflix,youtube,primevideo,hbo,disney,roku,spotify,twitch,xvideos,pornhub,dmm,dlsite,telegram,speedtest,openai,playstation,nintendo,xbox,ea,epicgames,ubisoft", "Lists to be exported in plaintext format, separated by ',' comma")
 	excludeAttrs = flag.String("excludeattrs", "cn@!cn@ads,geolocation-cn@!cn@ads,geolocation-!cn@cn@ads", "Exclude rules with certain attributes in certain lists, seperated by ',' comma, support multiple attributes in one list. Example: geolocation-!cn@cn@ads,geolocation-cn@!cn")
 	toGFWList    = flag.String("togfwlist", "geolocation-!cn", "List to be exported in GFWList format")
 )
